ca804087ceae3fb85d826ec70d30b3cf230a9c0f
Zzx6047 <474969612@qq.com>
2019-12-26 00:13:33 +0800

    环境设置

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,11 @@
 import storage from './storage/index'
 export default {
   mounted() {
-    // storage.setItem("a", 1)
+    storage.setItem("a", 1)
     // storage.setItem("user", {a: 1})
     // storage.setItem("abc", {a: 1}, 'user')
     // storage.clear('a')
-    storage.clear('a', 'user')
+    // storage.clear('a', 'user')
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+// import VueAxios from 'vue-axios'
 import store from './store'
+import env from './env'
+
+axios.defaults.baseURL = env.baseURL;
+axios.defaults.timeout = 8000;
+axios.interceptors.response.use(function(response) {
+  let res = response.data;
+  if(res.status == 0) {
+    return res.data;
+  }else if(res.status == 10) {
+    window.location.href = '/#/login'
+  }else {
+    alert(res.msg)
+  }
+})
 
 Vue.config.productionTip = false
 
